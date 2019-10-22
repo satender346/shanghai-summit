01bc9db59892db9e07bc5de05d7cccb1fc57aac4
Ian Howell <ian.howell0@gmail.com>
2019-10-22 10:44:55 -0500

    Add a completed operator
    This will need to be chunked up into meaningful tagged commits to act as
    "checkpoints" through a demo

---
+++ summit-operator/pkg/apis/cloner/group.go
@@ -0,0 +1,6 @@
+// Package cloner contains cloner API versions.
+//
+// This file ensures Go source parsers acknowledge the cloner package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package cloner
